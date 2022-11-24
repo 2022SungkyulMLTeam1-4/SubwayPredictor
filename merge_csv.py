d3c8222a1fda9d83c6ab05f1c7b72f84f58c5fab
choiseyeon <choi20014830@gmail.com>
2022-11-24 10:04:26 +0900

    feat: 기존에 수집한 xlsx 파일을 csv로 모두 병합 코드 추가

---
+++ merge_csv.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+
+file_path = "파일 경로"
+file_list = os.listdir(file_path)
+
+for file_name in file_list:
+    print(file_name)
+    data = pd.read_excel(file_path+"/"+file_name)
+    
+    db=pd.DataFrame(data, columns=['전체 지하철 수', 'n번째 지하철', '사소한 항목', '지하철호선ID', '지하철호선명', '지하철역ID',
+               '지하철역명', '열차번호', '최종수신날짜', '최종수신시간', '상하행선구분', '종착지하철역ID',
+               '종착지하철역명', '열차상태구분', '급행여부', '막차여부'])
+    
+    db.to_csv('datacsv.csv', mode='a', header=False, index=True)
