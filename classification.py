e0fe91355f57d0e860ad49adcc3852515d2f96ba
osm1892 <osm189214@gmail.com>
2022-11-29 00:35:00 +0900

    feat: seq2seq 모델 임시 구성

--- classification.py
+++ classification.py
@@ -1,3 +1,5 @@
+import random
+
 import pandas as pd
 import torch
 import torch.nn as nn
@@ -17,7 +19,7 @@ class SubwayDataset(Dataset):
         for i, val in data:
             self.data.append(torch.tensor(val.values))
 
-        lengths = list(map(len, self.data))
+        self.lengths = list(map(len, self.data))
         self.data = torch.nn.utils.rnn.pad_sequence(self.data)
 
     def __len__(self):
@@ -27,14 +29,86 @@ class SubwayDataset(Dataset):
         pass
 
 
-class Model(nn.Module):
-    """
-    학습용 모델입니다.
-    """
-
-    def __init__(self):
+class Encoder(nn.Module):
+    def __init__(self, input_dim, embedding_dim, hidden_dim, n_layers, dropout):
         super().__init__()
-        self.lstm = nn.LSTM(100, 200, 5, batch_first=True)
 
-    def forward(self, x):
-        x = self.lstm(x)
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+
+        self.embedding = nn.Embedding(input_dim, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, dropout=dropout)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src):
+        embedded = self.dropout(self.embedding(src))
+
+        outputs, (hidden, cell) = self.lstm(embedded)
+
+        return hidden, cell
+
+
+class Decoder(nn.Module):
+    def __init__(self, output_dim, embedding_dim, hidden_dim, n_layers, dropout):
+        super().__init__()
+
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+
+        self.embedding = nn.Embedding(output_dim, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, dropout=dropout)
+        self.fc_out = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, input_, hidden, cell):
+        input_ = input_.unseueeze(0)
+
+        embedded = self.dropout(self.embedding(input_))
+
+        output, (hidden, cell) = self.lstm(embedded, (hidden, cell))
+
+        prediction = self.fc_out(output.squeeze(0))
+
+        return prediction, hidden, cell
+
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+        assert encoder.hidden_dim == decoder.hidden_dim, "hidden dimensions of encoder decoder isn't equal"
+        assert encoder.n_layers == decoder.n_layers, "layer of encoder decoder isn't equal"
+
+    def forward(self, src, trg, teacher_forcing_ratio=0.5):
+        batch_size = trg.shape[1]
+        trg_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)
+
+        hidden, cell = self.encoder(src)
+
+        # 첫 번째 입력값 <sos> 토큰
+        input_ = trg[0, :]
+
+        for t in range(0, trg_len):
+            output, hidden, cell = self.decoder(input_, hidden, cell)
+
+            # prediction 저장
+            outputs[t] = output
+
+            # teacher forcing을 사용할지, 말지 결정
+            teacher_force = random.random() < teacher_forcing_ratio
+
+            # 가장 높은 확률을 갖은 값 얻기
+            top1 = output.argmax(1)
+
+            # teacher forcing의 경우에 다음 lstm에 target token 입력
+            input_ = trg[t] if teacher_force else top1
+
+        return outputs
